420b5383f968b8e76225fa66dfa25188ce7c8c55
Monika Pietyszuk <M.Pietyszuk@tt.com.pl>
2018-03-05 09:41:40 +0100

    Initial commit - 4th tutorial.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'app.bundle.js'
+    },
+    module:{
+      rules: [
+           { test: /\.scss$/, use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: [ 'css-loader','sass-loader'] }) },
+      ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Project Demo',
+        // minify: {
+        //     collapseWhitespace: true
+        // },
+        hash: true,
+        template: './src/index.ejs'
+    }),
+    new ExtractTextPlugin({
+        filename: "app.css",
+        disable: false,
+        allChunks: true
+    })]
+}
